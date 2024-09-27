bc47da715daada36a967de9fb482f13ac8691df3
Learners506 <1985173316@qq.com>
2024-09-27 20:12:47 +0800

    1

--- ZZZIFOX/ProjectItems/Translater.cs
+++ ZZZIFOX/ProjectItems/Translater.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZZZIFOX.WPFUI;
 
 namespace ZZZIFOX.ProjectItems
 {
     public class Translater
     {
+        [CommandMethod(nameof(TRANS))]
+        public void TRANS()
+        {
+            while (true)
+            {
+                PromptKeywordOptions pko = new PromptKeywordOptions("\n设置[S]");
+                pko.Keywords.Add("S");
+                pko.Keywords.Default = "S";
+                pko.AppendKeywordsToMessage = false;
+
+                var pr = Env.Editor.GetKeywords(pko);
+   
+                if (pr.Status == PromptStatus.OK && pr.StringResult == "S")
+                {
+                    new TranslateUI().ShowDialog();
+                    
+                }
+                else if (pr.Status == PromptStatus.Cancel)
+                {
+                    // 用户输入ESC退出
+                    break;
+                }
+                else
+                {
+
+                }
+
+            }
 
 
 
+
+        }
     }
 }
