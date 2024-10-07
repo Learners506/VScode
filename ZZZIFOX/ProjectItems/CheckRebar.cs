0399b7ed89828054d1e32bb461fa5c2d6219844d
Learners506 <1985173316@qq.com>
2024-10-07 12:13:08 +0800

    1

---
+++ ZZZIFOX/ProjectItems/CheckRebar.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.AutoCAD.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Integration;
+using ZZZIFOX.WPFUI;
+
+namespace ZZZIFOX.ProjectItems
+{
+    public class CheckRebar
+    {
+        private static PaletteSet pset;
+        [CommandMethod(nameof(REB))]
+        public static void REB()
+        {
+            // pset初始化
+            if (pset is null)
+            {
+                pset = new PaletteSet("梁钢筋构造");
+                var wpf = new Rebar();
+                //wpf进行转换
+                var host = new ElementHost()
+                {
+                    AutoSize = true,
+                    Dock = DockStyle.Fill,
+                    Child = wpf,
+                };
+                pset.Add("abc", host);
+                pset.Visible = true;
+                //pset.Dock = DockSides.Bottom;
+                //pset.Size = new Size(100, 500);
+                return;
+            }
+            // 如果是存在的情况下的话
+            pset.Visible = !pset.Visible;
+        }
+    }
+}
---
+++ ZZZIFOX/WPFUI/Rebar.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZZZIFOX.WPFUI
+{
+    /// <summary>
+    /// Rebar.xaml 的交互逻辑
+    /// </summary>
+    public partial class Rebar : System.Windows.Controls.UserControl
+    {
+        public Rebar()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateTextBoxesAndDataGrids();
+        }
+
+
+        private void UpdateTextBoxesAndDataGrids() 
+        {
+            string 环境类别 = comboBoxHJLB.Text;
+            // 设置最小保护层
+            switch (环境类别)
+            {
+                case "一":
+                    textBoxZXBHC.Text = "20";
+                    break;
+                case "二a":
+                    textBoxZXBHC.Text = "25";
+                    break;
+                case "二b":
+                    textBoxZXBHC.Text = "35";
+                    break;
+                case "三a":
+                    textBoxZXBHC.Text = "40";
+                    break;
+                case "三b":
+                    textBoxZXBHC.Text = "50";
+                    break;
+            }
+            string 使用年限 = comboBoxSYNX.Text;
+            string 混凝土强度 = comboBoxHNTQD.Text;
+            string 最小保护层 = textBoxZXBHC.Text;
+            if (使用年限 == "50" && 混凝土强度 == "小等于C25")
+            {
+                textBoxBHCHD.Text = Convert.ToString(Convert.ToDouble(最小保护层) + 5);
+            }
+            else if (使用年限 == "50" && 混凝土强度 == "大于C25")
+            {
+                textBoxBHCHD.Text = 最小保护层;
+            }
+            else if (使用年限 == "100" && 混凝土强度 == "小等于C25")
+            {
+                textBoxBHCHD.Text = Convert.ToString(Convert.ToDouble(最小保护层) * 1.4 + 5);
+            }
+            else if (使用年限 == "100" && 混凝土强度 == "大于C25")
+            {
+                textBoxBHCHD.Text = Convert.ToString(Convert.ToDouble(最小保护层) * 1.4);
+            }
+            else
+            {
+                textBoxBHCHD.Text = "0"; // 默认值
+            }
+
+        }
+
+        
+    }
+}
